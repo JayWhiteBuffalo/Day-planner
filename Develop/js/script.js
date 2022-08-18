e1ef8faa839e8b97e1e421c894f4cfe38c32a815
White Buffalo <apothocare@gmail.com>
2022-08-17 23:17:01 -0500

    Rows generate properly and are visually balenced. Fixed bug  where extra divs where being created.

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -2,6 +2,11 @@
 //when page loads start 
 
 $(document).ready(function() {
+
+
+
+    //Array for all the task
+    taskArr = new Array(12);
 //loop will run untill 19th hour reached
      for (let hour = 7; hour <= 19; hour++ ){
              //index for array offset by hour
@@ -9,36 +14,71 @@ $(document).ready(function() {
 //adds rows to plannerContainer
         let $newRow = $('<div>');
          $newRow.addClass('row');
+         $newRow.addClass('flex');
          //giving each row created its own hour
          $newRow.attr('hour-index', hour);
          //display created row on page
          $("#plannerContainer").append($newRow);
      
 
-    
+//Start task time space    
 //create div where time can be put
          let $timeDiv = $('<div>');
          $timeDiv.addClass('time-block');
-         $(".row").append($timeDiv);
+         $newRow.append($timeDiv);
+         debugger
 //create timeBox
     const $timeBoxtxt = $('<span>');
         $timeBoxtxt.attr('class','timeBox');
         $timeBoxtxt.attr('id','timeBoxSpan');
-        $(".time-block").append($timeBoxtxt);
-
+        $timeBoxtxt.addClass('timeBox');
+        $newRow.append($timeBoxtxt);
+//Display the time
         $timeBoxtxt.text(`${hour}`);
 
-    
+//End Task Time space
+//input task Tow space
+let $taskSpan = $('<input>');
+
+$taskSpan.attr('id',`input-${index}`);
+$taskSpan.attr('hour-index',index);
+$taskSpan.attr('type','text');
+  $taskSpan.attr('class','dailyPlan');
+
+  //access index from data array for hour
+  $taskSpan.val(taskArr[index]);
+
+ 
+    // create col to control width
+    let $taskWidth = $('<div>');
+    $taskWidth.addClass('taskBox');
+
+    // add col width and row component to row
+    $newRow.append($taskWidth);
+    $taskWidth.append($taskSpan);
+
+//End Task Row Space
+
+//Start save button portion
+//Create space
+let $saveBox = $('<div>');
+$saveBox.addClass('saveBox');
+
+let $saveBtn = $('<button>');
+$saveBtn.attr('id', 'saveBoxBtn');   
+    $newRow.append($saveBox);
+    $saveBox.append($saveBtn);
+
        
         
        
 
-        
-
+    let saveBtnEl = document.querySelector("#saveBoxBtn");
     
+    saveBtnEl.addEventListener("click", function(){console.log("touchdown");});
      }
-       
 
+     
 });
 
 
